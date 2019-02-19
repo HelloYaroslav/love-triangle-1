1ab44f60085a398688ee7902c0510f2d11fd5a10
Your Name <you@example.com>
2019-02-19 18:08:17 +0300

    passed

--- src/index.js
+++ src/index.js
@@ -3,5 +3,16 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
+  let count = 0;
+  for(let i = 0; i < preferences.length; i++){
+    debugger
+    if(preferences[preferences[preferences[i]-1]-1] === i+1){
+      count++
+    }
+    
+  }
   // your implementation
+  return Math.floor(count / 3);
 };
+
+console.log(module.exports([6, 14, 12, 21, 3, 20, 7, 15, 16, 16, 14, 4, 11, 13, 18, 2, 1, 8, 3, 1, 12]))
